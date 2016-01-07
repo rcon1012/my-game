b2f0c3955f5708adf3abfc586f14f9b8952ab887
rcon1012 <rcon1012@gmail.com>
2016-01-06 23:44:03 -0500

    basic platforming

--- resources/scripts/game.js
+++ resources/scripts/game.js
@@ -1,37 +1,48 @@
 /**
  * Created by Ryan on 12/22/2015.
  */
-const gameHeight = 360;
-const gameWidth = 480;
-var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, "game-canvas", { preload: preload, create: create, update: update, render: render });
+const GAME_HEIGHT = 480;
+const GAME_WIDTH = 640;
+
+const CHARACTER_WIDTH = 50;
+const CHARACTER_HEIGHT = 100;
+
+const ROCKET_HEIGHT = 25;
+const ROCKET_WIDTH = 172;
+const NUM_ROCKETS = 10;
+var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, "game-canvas", { preload: preload, create: create, update: update, render: render });
 
 function preload () {
     game.load.image('character', 'resources/img/character.png');
     game.load.image('rocket', 'resources/img/rocket.png');
 }
 var character;
-var rockets
+var rockets;
 var wasd;
 var jumpButton;
+var pauseKey;
 function create () {
     // create world/physics
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.arcade.gravity.y = 300;
 
     // add character to game
-    character = game.add.sprite(game.world.centerX, game.world.centerY, 'character');
+    character = game.add.sprite(game.world.centerX/2, game.world.centerY/2, 'character');
     game.physics.enable(character, Phaser.Physics.ARCADE);
+    // TODO: change collideWorldBounds to false
     character.body.collideWorldBounds = true;
-    character.body.gravity.y = 1000;
+    character.body.gravity.y = 500;
     character.body.maxVelocity.y = 500;
     character.body.drag.x = 400;
 
     // add rockets
     rockets = game.add.physicsGroup(Phaser.Physics.ARCADE);
-    rockets.create(game.world.centerX + 150, game.world.centerY + 150, "rocket");
+    // rocket character lands on
+    rockets.create(game.world.centerX, game.world.centerY/2 + CHARACTER_HEIGHT + 230, "rocket");
     rockets.setAll("body.immovable", true);
     rockets.setAll("body.allowGravity", false);
     rockets.setAll("body.velocity.x", -100);
+
 
 
     // initialize controls
@@ -42,12 +53,30 @@ function create () {
         right: game.input.keyboard.addKey(Phaser.Keyboard.D),
     };
     jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    pauseKey = game.input.keyboard.addKey(Phaser.Keyboard.P);
+    pauseKey.onDown.add(togglePause, game);
+}
+
+
+function togglePause() {
+    game.physics.arcade.isPaused = (game.physics.arcade.isPaused) ? false : true;
 }
 
 function update() {
+    // spawn rockets
+    if(game.rnd.between(1, 100) % 100 == 0) {
+        var rocketY = game.rnd.between(0 + CHARACTER_HEIGHT, GAME_HEIGHT - ROCKET_HEIGHT);
+        rockets.create(game.world.width, rocketY, "rocket");
+        rockets.setAll("body.immovable", true);
+        rockets.setAll("body.allowGravity", false);
+        rockets.setAll("body.velocity.x", -100);
+    }
+
+
+
     // collision with rockets
     var isColliding = game.physics.arcade.collide(character, rockets);
-
     character.body.acceleration.y = 0;
     // character controls
     if (wasd.left.isDown)
@@ -74,5 +103,9 @@ function update() {
 }
 
 function render() {
+
+}
+
+function rocketCollisionHandler() {
 
 }
